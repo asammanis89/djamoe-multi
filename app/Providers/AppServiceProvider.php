f36d0e212753eb05f91c223a6c83c95b255cfb14
DESKTOP-6AGOSVB\USER <asammanis8900@gmail.com>
2025-11-24 22:29:04 +0700

    Add force HTTPS logic

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Providers; // <--- INI HARUS DI ATAS
+namespace App\Providers;
 
-use Illuminate\Support\Facades\URL; // <--- PINDAH KE SINI
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // --- INI KODE PENTINGNYA ---
+        // Kita paksa website pakai HTTPS agar Cloudflare tidak error Mixed Content
+        URL::forceScheme('https');
     }
 }
