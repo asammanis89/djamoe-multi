79c205967f9f56f3a3b2cd359d88a71f86f9ecce
DESKTOP-6AGOSVB\USER <asammanis8900@gmail.com>
2025-12-07 12:02:59 +0700

    "new update vite"

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,27 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/css/pages/home.css',
+                'resources/css/pages/produk.css',
+                'resources/css/pages/aktivitas.css',
+                'resources/css/pages/about.css',
+                'resources/css/pages/outlet.css',
+                'resources/js/pages/produk-ajax.js',
+                'resources/js/pages/aktivitas-modal.js',
+                'resources/js/pages/outlet.js',
+            ],
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'public/build',
+        emptyOutDir: true,
+        manifest: true,
+        minify: 'terser',
+    },
     server: {
         host: '0.0.0.0', // Memaksa Vite mendengarkan di semua network interface
         hmr: {
